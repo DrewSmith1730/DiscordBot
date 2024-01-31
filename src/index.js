1c6b8bd733f91934f86ac4b2035decfb9d33e426
DrewSmith1730 <drewsmith1730@gmail.com>
2024-01-31 02:55:53 -0600

    added my sql to the dependecies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const dotenv = require("dotenv");
 const fs = require('node:fs');
 const path = require('node:path');
+
 dotenv.config();
 
 const { Client, IntentsBitField } = require("discord.js");
 
 const client = new Client ({
     intents: [
+        // bitfields mean the parts of the server(guild) that the bot can access
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
@@ -15,24 +17,25 @@ const client = new Client ({
     ],
 });
 
+// command handling
 client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
+// default
 client.on('ready', (c) => {
     console.log(`${c.user.tag} is online.`);
 
 });
 
-// embed construction
-// embed goals (diff color for the embed depending on the day of the week,
-//              each team is gonna be a variable (mutually exclusive (cant be in one if in another)),
-//              
-//              )
-client.on('messageCreate', (msg) => {
-    // if message == "/creatembed"
-    
+// command function
+client.on(Events.InteractionCreate, interaction => {
+    // not all interations are chat commands this filters it to only use the chat commands
+	if (!interaction.isChatInputCommand()) return;
+	console.log(interaction);
 });
 
-
+// basic message reader
 client.on('messageCreate', (message) => {
     console.log(message);
     
