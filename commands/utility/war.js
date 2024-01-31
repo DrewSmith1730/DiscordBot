85764da9de9c4319c1defc8ae54724eae0926ccc
DrewSmith1730 <drewsmith1730@gmail.com>
2024-01-31 03:08:15 -0600

    part of the functionalitly required for adding custom slash commmands has been implimented

---
+++ commands/utility/war.js
@@ -0,0 +1,9 @@
+// this is going to create a embed for the collection of players that will be attending a given war
+// this will create the embed not update it later on
+
+// create a embed with 6 variable for { main1, main2, flex, def, tenetive, absent }
+// based on a player id it will load the class of the player next to their name in the list
+// 
+
+const { SlashCommandBuilder } = require('discord.js');
+
