3c3c781c23a4b023c09d0b8b822e3fb04c01fa9f
DrewSmith1730 <drewsmith1730@gmail.com>
2024-01-31 02:50:56 -0600

    starting to add commands to the bot

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Disband Discord Bot
 ## Chat bot designed to help organize a large guild of players for multiple concurrent events over the course of eache week
 ## Basic security done through env variables along
+
+
+
+
+##### TODO
+###### Character icons [✓]
+###### embed template for wars (main, main2, flex, defense, tenetive, absent)
+###### embed template for gear posts (name, gs, )
+###### 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const dotenv = require("dotenv");
+const fs = require('node:fs');
+const path = require('node:path');
 dotenv.config();
 
 const { Client, IntentsBitField } = require("discord.js");
@@ -8,8 +10,32 @@ const client = new Client ({
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildMessageReactions
     ],
+});
+
+client.commands = new Collection();
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online.`);
+
+});
+
+// embed construction
+// embed goals (diff color for the embed depending on the day of the week,
+//              each team is gonna be a variable (mutually exclusive (cant be in one if in another)),
+//              
+//              )
+client.on('messageCreate', (msg) => {
+    // if message == "/creatembed"
+    
+});
+
+
+client.on('messageCreate', (message) => {
+    console.log(message);
+    
 });
 
 client.login(process.env.DISCORD_TOKEN)
